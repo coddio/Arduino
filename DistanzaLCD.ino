6e0c76e951c58131dc6284d8521200bd5d099a1a
FeelCr3epy <filipponarici@gmail.com>
2019-03-02 09:03:16 +0100

    Update DistanzaLCD.ino

--- DistanzaLCD.ino
+++ DistanzaLCD.ino
@@ -1,16 +1,22 @@
+//Lo scopo è di misurare una distanza e mostrarla sull'lcd
+//Include la libreria per lo schermo
 #include <LiquidCrystal.h>
+//Stabilisce i pin dell'lcd
 int rs=12;
 int e=11;
 int D4=5;
 int D5=4;
 int D6=3;
 int D7=2;
+//Stabilisce i pin del sensore
 int triggerPin=9;
 int echoPin=10;
+//Stabilisce le variabili usate dal sensore
 long duration;
 int distance;
-//buzzpin attiva anche il led
+//Inizializza buzzpin che attiva anche il led
 int buzzPin=8;
+//Inizializza l'lcd con i pin stabiliti sopra
 LiquidCrystal lcd(rs,e,D4,D5,D6,D7);
 void setup()
 {
@@ -18,7 +24,10 @@ void setup()
   pinMode(triggerPin,OUTPUT);
   pinMode(echoPin,INPUT);
   pinMode(buzzPin,OUTPUT);
+  
+  //Inizializza la console
   Serial.begin(9600);
+  
   //inizializza l'lcd con le seguenti dimensioni
   lcd.begin(16,2);
   lcd.print("Dio bufu");
@@ -43,15 +52,20 @@ void loop()
   Serial.println(distance);
   lcd.print("Distanza: ");
   lcd.setCursor(0,1);
+  
   //Se la distanza è > di 500 dà errore
   if (distance <=500)
   {
+    //Spegne il led e il buzzer se accesi
     digitalWrite(buzzPin,LOW);
+    //Mostra sull'lcd la distanza
     lcd.print(distance);
   }
   else
   {
+    //Accende il buzzer e il led
     digitalWrite(buzzPin,HIGH);
+    //Mostra un errore sul lcd
     lcd.print("errore");
   }
   //Delay finale
